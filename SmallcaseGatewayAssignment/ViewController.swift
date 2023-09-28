d222020960da15034c830ef2e1447a79dde8ec66
Noor Bhatia <bhatia.noor01@gmail.com>
2023-09-28 11:29:45 +0530

    Initial Commit

---
+++ SmallcaseGatewayAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmallcaseGatewayAssignment
+//
+//  Created by noor on 28/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
